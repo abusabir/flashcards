628aafaeeefd424e2b3697671d178932a874d1a9
abusabir <olegagapitov@gmail.com>
2015-11-30 21:14:17 +0200

    crud begans

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,19 @@ class CardsController < ApplicationController
     def index
         @cards = Card.all
     end
+
+    def create
+        
+        # @card = Card.new(params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
+
+    def show
+        @card = Card.find(params[:id])
+    end
+
+    def new
+    end
+
+    def edit
+    end
 end
