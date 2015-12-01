86776b4f7f85ea2aa5ce4386f19e4b720561a944
abusabir <olegagapitov@gmail.com>
2015-12-01 18:49:34 +0200

    other methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,13 +4,21 @@ class CardsController < ApplicationController
     end
 
     def create
-        @card = Card.new(params.require(:card).permit(:original_text, :translated_text))
+        @card = Card.new(card_params)
         @card.review_date = DateTime.now.next_day(3)
-
         @card.save
 
         redirect_to @card
+    end
 
+    def update
+        @card = Card.find(params[:id])
+        @card.review_date = DateTime.now.next_day(3)
+        if @card.update(card_params)
+            redirect_to @card
+        else
+            render 'edit'
+        end
     end
 
     def show
@@ -21,5 +29,18 @@ class CardsController < ApplicationController
     end
 
     def edit
+        @card = Card.find(params[:id])
     end
+
+    def destroy
+        @card = Card.find(params[:id])
+        @card.destroy
+
+        redirect_to cards_path
+    end
+
+    private
+        def card_params
+            params.require(:card).permit(:original_text, :translated_text)
+        end
 end
