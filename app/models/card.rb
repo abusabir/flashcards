ba7c56c58a353b950794f5738596f2ea2da69845
abusabir <olegagapitov@gmail.com>
2015-12-05 20:14:23 +0200

    cards validator

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,12 @@ class Card < ActiveRecord::Base
     validates :original_text, presence: true
     validates :translated_text, presence: true
     validates :review_date, presence: true
+    validate :check_cards
+
+    def check_cards
+         if original_text = translated_text 
+            errors.add(:translated_text, "can't be the same as original text")    
+            errors.add(:original_text, "can't be the same as translated text")             
+         end
+    end
 end
