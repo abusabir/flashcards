75e4cd5db1a2fcefa6515e1983a4435e437b63c3
abusabir <olegagapitov@gmail.com>
2015-12-06 08:09:28 +0200

    edited

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,51 +1,50 @@
 class CardsController < ApplicationController
-    before_filter :find_card,
-        :only => [:show, :update, :edit, :destroy]
+  before_filter :find_card,
+      :only => [:show, :update, :edit, :destroy]
 
-    def index
-        @cards = Card.all
+  def index
+    @cards = Card.all
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.review_date = DateTime.now.next_day(3)
+    if @card.save
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text)
     end
 
-    def create
-        @card = Card.new(card_params)
-        @card.review_date = DateTime.now.next_day(3)
-        if @card.save
-            redirect_to @card
-        else
-            render 'new'
-        end
+    def find_card
+      @card = Card.find(params[:id])
     end
-
-    def update
-        if @card.update(card_params)
-            redirect_to @card
-        else
-            render 'edit'
-        end
-    end
-
-    def show
-    end
-
-    def new
-        @card = Card.new
-    end
-
-    def edit
-    end
-
-    def destroy
-        @card.destroy
-
-        redirect_to cards_path
-    end
-
-    private
-        def card_params
-            params.require(:card).permit(:original_text, :translated_text)
-        end
-
-        def find_card
-            @card = Card.find(params[:id])
-        end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,11 @@
 class Card < ActiveRecord::Base
-    validates :original_text, presence: true
-    validates :translated_text, presence: true
-    validates :review_date, presence: true
-    validate :check_cards
+  validates :original_text, :translated_text, :review_date,  presence: true
+  validate :check_cards
 
-    def check_cards
-         if original_text.casecmp(translated_text) == 0
-            errors.add(:translated_text, "can't be the same as original text")    
-            errors.add(:original_text, "can't be the same as translated text")             
-         end
+  def check_cards
+    if original_text.casecmp(translated_text) == 0
+      errors.add(:translated_text, "can't be the same as original text")    
+      errors.add(:original_text, "can't be the same as translated text")             
     end
+  end
 end
